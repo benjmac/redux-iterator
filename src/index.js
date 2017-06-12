b4072e2e2a7690b50c945b91b16767bb1f17ce69
Ben McCain <mccain.ben2@gmail.com>
2017-06-12 15:03:43 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const reduxIterator = ({dispatch}) => next => action => {
+  if (action[Symbol.iterator] && typeof action !== 'string') {
+    if (action instanceof Map) {
+      for (let elem of action) {
+        dispatch(elem[1])
+      }
+    }
+    else {
+      for (let elem of action) {
+        dispatch(elem)
+      }
+    }
+  }
+  else if (typeof action === 'object' && action !== null && !action.type) {
+    for (let key in action) {
+      if (typeof action[key] === 'object' && action[key] !== null) {
+        dispatch(action[key]);
+      }
+    }
+  }
+  else {
+    next(action);
+  }
+}
+
+export default reduxIterator;
